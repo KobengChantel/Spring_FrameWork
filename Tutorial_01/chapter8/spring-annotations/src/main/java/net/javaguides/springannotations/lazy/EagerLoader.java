26067c7b90873c93c8ca57e701ac876425a984a9
KobengChantel <kobengchantel@gmail.com>
2024-11-07 12:58:19 +0200

    done 9

---
+++ Tutorial_01/chapter8/spring-annotations/src/main/java/net/javaguides/springannotations/lazy/EagerLoader.java
@@ -0,0 +1,12 @@
+package net.javaguides.springannotations.lazy;
+
+
+import org.springframework.stereotype.Component;
+
+@Component
+
+public class EagerLoader {
+    public EagerLoader() {
+        System.out.println("EagerLoader ..");
+    }
+}
---
+++ Tutorial_01/chapter9/spring-annotations/src/main/java/net/javaguides/springannotations/controller/BookController.java
@@ -0,0 +1,23 @@
+package net.javaguides.springannotations.controller;
+
+import net.javaguides.springannotations.beans.Book;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class BookController {
+
+    @RequestMapping("/hello-world")
+    @ResponseBody
+    public String helloWorld(){
+        return "Hello World!";
+    }
+
+    @RequestMapping("/book")
+    @ResponseBody
+    public Book getBook(){
+        Book book = new Book(1, "Core Java", "Learn Core Java and latest Features");
+        return book;
+    }
+}
