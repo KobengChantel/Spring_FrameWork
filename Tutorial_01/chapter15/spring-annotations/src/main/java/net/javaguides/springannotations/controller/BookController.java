9b0d360bcaa7981c6de21af026d54525c260bf06
Kobeng Chantel <kobengchantel@gmail.com>
2024-11-08 11:25:00 +0200

    Tutorial 1 done

--- Tutorial_01/chapter15/spring-annotations/src/main/java/net/javaguides/springannotations/controller/BookController.java
+++ Tutorial_01/chapter15/spring-annotations/src/main/java/net/javaguides/springannotations/controller/BookController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.http.HttpStatus;
 
 
@@ -72,7 +73,14 @@ public class BookController {
         updateBook.setId(id);
         return ResponseEntity.ok().body(updateBook);
     }
-
+//using @Delete Annotation
+    @DeleteMapping(value = "/books/delete/{id}")
+//    @RequestMapping( value = "/books/delete/{id}", method = RequestMethod.DELETE)
+    public ResponseEntity<String> deleteBook( @PathVariable
+            int id){
+        System.out.println(id);
+        return ResponseEntity.ok().body("Book Deleted Successfully!.");
+    }
     }
 
 
