a7412ac6c2b48d9514b4e9a21a25f459ef9b6d74
Kobeng Chantel <kobengchantel@gmail.com>
2025-08-05 22:17:34 +0200

    adding comments

--- my_work/chp01/taco-cloud/src/main/java/tacos/TacoCloudApplication.java
+++ my_work/chp01/taco-cloud/src/main/java/tacos/TacoCloudApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class TacoCloudApplication {
-
+//our taco cloud application where  we will tun our program
 	public static void main(String[] args) {
 		SpringApplication.run(TacoCloudApplication.class, args);
 	}
