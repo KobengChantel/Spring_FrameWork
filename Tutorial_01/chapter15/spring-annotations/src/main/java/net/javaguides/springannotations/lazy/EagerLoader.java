bff5a3cc30a59897562d8849da16cb3756e18bb3
Kobeng Chantel <kobengchantel@gmail.com>
2025-09-08 19:58:51 +0200

    adding comments

--- Tutorial_01/chapter15/spring-annotations/src/main/java/net/javaguides/springannotations/lazy/EagerLoader.java
+++ Tutorial_01/chapter15/spring-annotations/src/main/java/net/javaguides/springannotations/lazy/EagerLoader.java
@@ -1,11 +1,13 @@
 package net.javaguides.springannotations.lazy;
 
-
 import org.springframework.stereotype.Component;
 
-@Component
+// This class demonstrates eager initialization in Spring; the bean is created as soon as the application context loads.
 
+@Component
 public class EagerLoader {
+
+    // Constructor prints a message when the bean is instantiated
     public EagerLoader() {
         System.out.println("EagerLoader ..");
     }
