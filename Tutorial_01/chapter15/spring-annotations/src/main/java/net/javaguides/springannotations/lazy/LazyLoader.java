ae86b388c382d156928d3b8238f31b18f11b9d3e
Kobeng Chantel <kobengchantel@gmail.com>
2025-09-09 20:47:14 +0200

    spring

--- Tutorial_01/chapter15/spring-annotations/src/main/java/net/javaguides/springannotations/lazy/LazyLoader.java
+++ Tutorial_01/chapter15/spring-annotations/src/main/java/net/javaguides/springannotations/lazy/LazyLoader.java
@@ -3,12 +3,14 @@ package net.javaguides.springannotations.lazy;
 import org.springframework.stereotype.Component;
 import org.springframework.context.annotation.Lazy;
 
+// This class demonstrates lazy initialization in Spring; the bean is created only when it is first requested.
+
 @Component
-//loading spring bean of this cass on demand
-@Lazy
+@Lazy  // Marks this bean to be loaded on demand rather than at application startup
 public class LazyLoader {
 
-    public LazyLoader(){
+    // Constructor prints a message when the bean is instantiated
+    public LazyLoader() {
         System.out.println("LazyLoader ..");
     }
 }
