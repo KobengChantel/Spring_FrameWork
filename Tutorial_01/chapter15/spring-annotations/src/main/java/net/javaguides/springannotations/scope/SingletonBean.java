7ca89f90d77a8c51f0fd0575062b841970e0c8bc
Kobeng Chantel <kobengchantel@gmail.com>
2025-09-13 06:51:19 +0200

    adding comments

--- Tutorial_01/chapter15/spring-annotations/src/main/java/net/javaguides/springannotations/scope/SingletonBean.java
+++ Tutorial_01/chapter15/spring-annotations/src/main/java/net/javaguides/springannotations/scope/SingletonBean.java
@@ -2,11 +2,15 @@ package net.javaguides.springannotations.scope;
 
 import org.springframework.stereotype.Component;
 
+// This class demonstrates a singleton-scoped Spring bean, which is created only once per Spring application context.
+
 @Component
+// By default, Spring beans are singleton-scoped, so the @Scope annotation is optional here
 //@Scope(value = ConfigurableBeanFactory.SCOPE_SINGLETON)
 public class SingletonBean {
 
-    public SingletonBean(){
+    // Constructor prints a message when the single instance is created
+    public SingletonBean() {
         System.out.println("SingletonBean..");
     }
 }
