c7e85e500448c3bdf1c180113f1f73d58409ee2e
Kobeng Chantel <kobengchantel@gmail.com>
2025-09-13 06:50:21 +0200

    adding comments

--- Tutorial_01/chapter15/spring-annotations/src/main/java/net/javaguides/springannotations/lazy/propertysource/PropertySourceDemo.java
+++ Tutorial_01/chapter15/spring-annotations/src/main/java/net/javaguides/springannotations/lazy/propertysource/PropertySourceDemo.java
@@ -4,43 +4,52 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Component;
 
+// This class demonstrates how to access application properties from the Environment in Spring.
+
 @Component
 public class PropertySourceDemo {
 
+    // Spring Environment object provides access to properties from application.properties or application.yml
     @Autowired
     private Environment environment;
 
-    //@Value("${gmail.host}")
+    // These fields could be injected directly using @Value, but are left commented for demonstration
+    // @Value("${gmail.host}")
     private String host;
 
-    //    @Value("${gmail.email}")
+    // @Value("${gmail.email}")
     private String email;
 
-    //  @Value("${gmail.password}")
+    // @Value("${gmail.password}")
     private String password;
 
-    //@Value("${app.name}")
+    // @Value("${app.name}")
     private String appName;
 
-    //@Value("${app.description}")
+    // @Value("${app.description}")
     private String appDesc;
 
+    // Get property "gmail.host" from application.properties or environment variables
     public String getHost() {
         return environment.getProperty("gmail.host");
     }
 
+    // Get property "gmail.email"
     public String getEmail() {
         return environment.getProperty("gmail.email");
     }
 
+    // Get property "gmail.password"
     public String getPassword() {
         return environment.getProperty("gmail.password");
     }
 
+    // Get property "app.name"
     public String getAppName() {
         return environment.getProperty("app.name");
     }
 
+    // Get property "app.description"
     public String getAppDesc() {
         return environment.getProperty("app.description");
     }
--- Tutorial_01/chapter15/spring-annotations/src/main/java/net/javaguides/springannotations/scope/PrototypeBean.java
+++ Tutorial_01/chapter15/spring-annotations/src/main/java/net/javaguides/springannotations/scope/PrototypeBean.java
@@ -4,11 +4,14 @@ import org.springframework.beans.factory.config.ConfigurableBeanFactory;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
+// This class demonstrates a prototype-scoped Spring bean, which creates a new instance every time it is requested.
+
 @Component
-@Scope(value = ConfigurableBeanFactory.SCOPE_PROTOTYPE)
+@Scope(value = ConfigurableBeanFactory.SCOPE_PROTOTYPE) // Marks this bean as prototype-scoped
 public class PrototypeBean {
 
-    public PrototypeBean(){
+    // Constructor prints a message whenever a new instance is created
+    public PrototypeBean() {
         System.out.println("PrototypeBean");
     }
 }
