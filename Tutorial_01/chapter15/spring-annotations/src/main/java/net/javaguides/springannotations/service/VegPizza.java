b56192146ab66d89fd1bbf8fe4cc90d3d9c673a3
Kobeng Chantel <kobengchantel@gmail.com>
2025-09-14 19:35:20 +0200

    adding comments

--- Tutorial_01/chapter15/spring-annotations/src/main/java/net/javaguides/springannotations/service/VegPizza.java
+++ Tutorial_01/chapter15/spring-annotations/src/main/java/net/javaguides/springannotations/service/VegPizza.java
@@ -1,10 +1,12 @@
 package net.javaguides.springannotations.service;
 
+// This class implements the Pizza interface to represent a vegetarian pizza type.
+
 public class VegPizza implements Pizza {
 
+    // Implementation of the getPizza() method from the Pizza interface
     @Override
-    public String getPizza()
-    {
+    public String getPizza() {
         return "Veg Pizza!";
     }
 }
